2396908ec1573be9fd9b9e99ff81d703f73a0a92
Rus K <ruswalten1@gmail.com>
2024-09-14 20:52:17 -0700

    prject 1 added

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,4 +1,44 @@
+import Image from "next/image"
+import Link from "next/link"
+
+const PROJECTS = [
+  {
+    title: "MapTask",
+    url: "maptask.ruslan.guru",
+    description:
+      "This service enables users to pin tasks onto a map or any image, providing detailed context for each task and serving as an interactive dashboard. Users can track the progress of their tasks, manage their personal accounts, handle billing, and customize settings, making it a comprehensive tool for visual task management and organization.",
+  },
+]
+
 const Portfolio = () => {
-  return <div>In progress</div>
+  return (
+    <div>
+      {PROJECTS.map(({ title, description, url }, idx) => (
+        <div className="-[8px] flex w-full flex-col items-center rounded bg-[#1b0e1d] p-4 md:flex-row md:items-start">
+          {/* Image on the left */}
+          <div className="w-full md:w-1/3">
+            <a target="_blank" href={`https://${url}`} rel="noopener noreferrer">
+              <Image
+                src="/images/portfolio/p1.png"
+                alt="Your image description"
+                width={500} // You can adjust the width and height as needed
+                height={500}
+                className="h-auto w-full"
+              />
+            </a>
+          </div>
+
+          {/* Text and description on the right */}
+          <div className="mt-4 w-full md:ml-4 md:mt-0 md:w-2/3">
+            <h2 className="text-xl font-semibold">
+              {title}
+              <span className="float-right">#{idx + 1}</span>
+            </h2>
+            <p className="mt-2">{description}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
 }
 export default Portfolio
